c270da3d14e234689a134e02ca4b5febce57354d
leonardo <leoleitao27@gmail.com>
2022-03-11 12:40:25 -0300

    a

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
@@ -9,5 +10,6 @@ route.get('/', homeController.index);
 
 //rotas de login
 route.get('/login/index',loginController.index);
+route.post('/login/register',loginController.register);
 
 module.exports = route;
--- server.js
+++ server.js
@@ -2,21 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTIONSTRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
-
-app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,7 +27,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
