2c54b8285e1504a4646a9e67d1eee81ee06bf6ce
leonardo <leoleitao27@gmail.com>
2022-03-23 13:01:55 -0300

    listando e apagando contatos

--- src/models/contatoModel.js
+++ src/models/contatoModel.js
@@ -53,17 +53,32 @@ Contato.prototype.cleanUp = function(){
   };
 }
 
+//metodos estáticos
+
+Contato.prototype.edit = async function(id) {
+  if(typeof id !== 'string') return;
+  this.valida();
+  if(this.errors.length > 0) return;
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+};
+
+// Métodos estáticos
 Contato.buscaPorId = async function(id) {
   if(typeof id !== 'string') return;
   const contato = await ContatoModel.findById(id);
   return contato;
 };
 
-Contato.prototype.edit = async function(id){
+Contato.buscaContatos = async function() {
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 });
+  return contatos;
+};
+
+Contato.delete = async function(id) {
   if(typeof id !== 'string') return;
-  this.valida();
-  if(this.errors.lenght > 0) return;
-  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true});
-}
+  const contato = await ContatoModel.findOneAndDelete({_id: id});
+  return contato;
+};
 
 module.exports = Contato;
