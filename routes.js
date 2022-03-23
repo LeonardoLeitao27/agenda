c2830345c59f55dc6149d8c455c3635db0763fe6
leonardo <leoleitao27@gmail.com>
2022-03-23 10:54:16 -0300

    cadastro de contato adicionado

--- routes.js
+++ routes.js
@@ -3,15 +3,24 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
-
+//importando os middlewares
+const { loginRequired } = require('./src/middlewares/middleware');
+ 
 // Rotas da home
 route.get('/', homeController.index);
 
 //rotas de login
-route.get('/login/index',loginController.index);
-route.post('/login/register',loginController.register);
-route.post('/login/login',loginController.login);
-route.get('/login/logout',loginController.logout);
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
+
+//rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+ 
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -20,3 +20,14 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+//verificando se o usuario está logado
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user){
+    req.flash('errors', 'Você precisa fazer login');
+    req.session.save(()=> res.redirect('/'));
+    return;
+    }
+
+    next();
+};
