3f1b37387ba9672c2b5259beefb853ad7f44681c
leonardo <leoleitao27@gmail.com>
2022-03-25 11:21:09 -0300

    navbar2 e autenticação

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
         }
       }
     }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      /* test: /\.css$/,
+      use: ['style-loader', 'css-loader'] */
     }]
   },
   devtool: 'source-map'
